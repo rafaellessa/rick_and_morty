8558844b9184a64fd99ec3d1539f58c8cee8db84
Rafael Lessa <rafael.lessa150@gmail.com>
2021-09-16 20:25:59 -0300

    chore: config packages

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
@@ -15,3 +16,20 @@ module.exports = {
     }),
   },
 };
+
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
