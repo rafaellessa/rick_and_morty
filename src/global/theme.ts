3402127819bf39174402bc9bfed5f8c2bafa1721
Rafael Lessa <rafael.lessa150@gmail.com>
2021-09-16 19:30:35 -0300

    feat: initial commit

---
+++ src/global/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    primary: '#1C1E45',
+    secondary: '#34A853',
+    secondary100: '#338A43',
+    secondary200: '#E7E7E7',
+    white: '#fff',
+  },
+  fonts: {
+    regular: 'Poppins-Regular',
+    medium: 'Poppins-Medium',
+    semiBold: 'Poppins-SemiBold',
+    bold: 'Poppins-Bold',
+  },
+};
