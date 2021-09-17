45e70bbc2f7a7ef34e43411ceb3b63e89c0a481d
Rafael Lessa <rafael.lessa150@gmail.com>
2021-09-17 19:06:52 -0300

    feat: implements person redux

---
+++ src/redux/types/types.person.ts
@@ -0,0 +1,50 @@
+import {Action} from 'redux';
+import {ImmutableObject} from 'seamless-immutable';
+
+export interface Person {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: 'masculino' | 'feminino';
+  location: string;
+  image: string;
+  episode: Array<string>;
+}
+
+export interface PersonActionTypes {
+  REQUEST_GET_ALL_PERSONS: string;
+  SUCCESS_GET_ALL_PERSONS: string;
+  FAILURE_GET_ALL_PERSONS: string;
+
+  RESET: string;
+}
+
+export interface State {
+  persons: Person[];
+  loading: boolean;
+  error: string | null;
+}
+
+export type PersonState = ImmutableObject<State>;
+
+export type RequestGetAllPersons = Action<PersonActionTypes>;
+
+export interface SuccessGetAllPersons extends Action<PersonActionTypes> {
+  persons: Person[];
+}
+
+export interface FailureGetAllPersons extends Action<PersonActionTypes> {
+  error: string | null;
+}
+
+export interface CreatorTypes {
+  requestGetAllPersons(): RequestGetAllPersons;
+  successGetAllPersons(persons: Person[]): SuccessGetAllPersons;
+  failureGetAllPersons(error: string | null): FailureGetAllPersons;
+}
+
+export type ReducerTypes = RequestGetAllPersons &
+  SuccessGetAllPersons &
+  FailureGetAllPersons;
