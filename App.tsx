6dc7ee8cf8b27f5fb831b1dc2b43790fdce3ce9f
Rafael Lessa <rafael.lessa150@gmail.com>
2021-09-17 19:03:58 -0300

    feat: configure store, reactotron in app

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
-
+import {StatusBar} from 'react-native';
+import {Provider} from 'react-redux';
 import {ThemeProvider} from 'styled-components/native';
 import theme from './src/global/theme';
-import Splash from './src/screens/Splash';
-import {StatusBar} from 'react-native';
+import store from './src/redux/store';
 import Home from './src/screens/Home';
 
 const App: React.FC = () => {
+  if (__DEV__) {
+    import('./ReactoTron').then(() => console.log('Reactotron Configurado'));
+  }
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar barStyle="light-content" />
-      <Home />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <StatusBar barStyle="light-content" />
+        <Home />
+      </ThemeProvider>
+    </Provider>
   );
 };
 
